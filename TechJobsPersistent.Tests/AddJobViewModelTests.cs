1f3a28762b71fa003db4c69b4a9a9a1ddbc564b0
Sally Steuterman <sally.steuterman@gmail.com>
2022-08-04 16:15:57 -0500

    Updated starter code

---
+++ TechJobsPersistent.Tests/AddJobViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TechJobsPersistentAutograded.Controllers;
+using TechJobsPersistentAutograded.ViewModels;
+using Xunit;
+
+namespace TechJobsPersistentAutograded.Tests
+{
+    public class AddJobViewModelTests
+    {
+        [Fact]
+        public void AddJobViewModel_ContainsCorrectNumberOfProperties()
+        {
+            //arrange
+            Type testAJVM = typeof(AddJobViewModel);
+            PropertyInfo[] infos = testAJVM.GetProperties();
+
+            //assert
+            Assert.Equal(5, infos.Length);
+            //Should have Name, EmployerId, Employers, SkillId, & Skills
+        }
+
+        [Fact]
+        public void AddJobViewModel_ContainsTwoConstructors()
+        {
+            Type testAJVM = typeof(AddJobViewModel);
+            ConstructorInfo[] cInfo = testAJVM.GetConstructors();
+            List<ConstructorInfo> cList = new List<ConstructorInfo>();
+
+            foreach(var c in cInfo)
+            {
+                cList.Add(c);
+            }
+
+            Assert.Equal(2, cInfo.Length);
+            // one should take a list of employers and a list of skills
+            // other should be empty
+            
+        }
+
+    }
+}
